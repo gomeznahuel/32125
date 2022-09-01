cc76c7019ccd0dd21e85bdf9c7d14c67f7bd2331
Nahuel Gómez <ngomezpf@gmail.com>
2022-09-01 14:46:50 -0300

    Desafío 02: Manejo de archivos

--- 01-Clases/typescript/index.ts
+++ 01-Clases/typescript/index.ts
@@ -1,40 +1,40 @@
-type BooksProps = {
-  title: string;
-  author: string;
-};
+// type BooksProps = {
+//   title: string;
+//   author: string;
+// };
 
-class User {
-  name: string;
-  surname: string;
-  books: BooksProps[];
-  pets: string[];
+// class User {
+//   name: string;
+//   surname: string;
+//   books: BooksProps[];
+//   pets: string[];
   
-  constructor( name: string, surname: string, books: BooksProps[], pets: string[] ) {
-    this.name = name;
-    this.surname = surname;
-    this.books = books;
-    this.pets = pets;
-  }
+//   constructor( name: string, surname: string, books: BooksProps[], pets: string[] ) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.books = books;
+//     this.pets = pets;
+//   }
 
-  getFullName(): string {
-    return `${this.name} ${this.surname}`;
-  }
+//   getFullName(): string {
+//     return `${this.name} ${this.surname}`;
+//   }
 
-  addPet(pet: string): void {
-    this.pets.push(pet);
-  }
+//   addPet(pet: string): void {
+//     this.pets.push(pet);
+//   }
 
-  countPets(): number {
-    return this.pets.length;
-  }
+//   countPets(): number {
+//     return this.pets.length;
+//   }
 
-  addBook(book: BooksProps): void {
-    this.books.push(book);
-  }
+//   addBook(book: BooksProps): void {
+//     this.books.push(book);
+//   }
 
-  getBooks(): string[] {
-    return this.books.map((book) => book.title);
-  }
-}
+//   getBooks(): string[] {
+//     return this.books.map((book) => book.title);
+//   }
+// }
 
-const firstUser = new User('Milo', 'Aukerman', [{ title: 'Book 1', author: 'Author 1' }, { title: 'Book 2', author: 'Author 2' }], ['cat', 'dog']);
+// const firstUser = new User('Milo', 'Aukerman', [{ title: 'Book 1', author: 'Author 1' }, { title: 'Book 2', author: 'Author 2' }], ['cat', 'dog']);
---
+++ 02-Manejo-de-archivos/database/index.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    name: 'Green Tea',
+    price: '2.50',
+    thumbnail: 'https://www.example.com/images/green-tea.jpg',
+  },
+  {
+    name: 'Black Tea',
+    price: '2.50',
+    thumbnail: 'https://www.example.com/images/black-tea.jpg',
+  },
+  {
+    name: 'Oolong Tea',
+    price: '2.50',
+    thumbnail: 'https://www.example.com/images/oolong-tea.jpg',
+  },
+];
+
+module.exports = products;
